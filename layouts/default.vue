98cd2c50d76bcaf097d0b91958eae99845230482
sonht <mikechael74@gmail.com>
2026-01-27 22:20:18 +0700

    fix layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,8 @@
 <template>
-  <div>
-    <!-- Body -->
-    <div class="w-full">
-      <ClientOnly fallback-tag="span" fallback="Loading comments...">
-        <slot></slot>
-      </ClientOnly>
-    </div>
-  </div>
+  <!-- Body -->
+  <ClientOnly fallback-tag="span" fallback="Loading comments...">
+    <slot></slot>
+  </ClientOnly>
 </template>
 
 <script setup>
