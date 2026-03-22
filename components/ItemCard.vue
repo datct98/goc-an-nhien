e00aa43790ad13ccf9ca89f499edc2be7e2a4fbe
sonht <mikechael74@gmail.com>
2026-03-22 22:28:57 +0700

    update Store

--- components/ItemCard.vue
+++ components/ItemCard.vue
@@ -7,19 +7,24 @@ const props = defineProps({
   title: String,
   subtitle: String,
   price: String,
-  icon: String
+  icon: String,
+  imagePath: String,
 })
 </script>
 
 <template>
   <div class="card" :class="rank">
-    
+
     <div class="rank-label">
       {{ rank }}
     </div>
 
-    <div class="icon">
+    <!-- <div class="icon">
       {{ icon }}
+    </div> -->
+
+    <div class="image">
+      <Image :src="imagePath" alt="Item" />
     </div>
 
     <div class="title">
@@ -38,112 +43,127 @@ const props = defineProps({
 </template>
 
 <style scoped>
-
-.card{
+.card {
   /* width:200px; */
-  padding:20px 10px;
-  border-radius:18px;
-  text-align:center;
-  position:relative;
+  padding: 20px 10px;
+  border-radius: 18px;
+  text-align: center;
+  position: relative;
 
-  background:linear-gradient(#3a1e08,#1a0c03);
-  border:2px solid rgba(255,255,255,0.1);
+  background: linear-gradient(#3a1e08, #1a0c03);
+  border: 2px solid rgba(255, 255, 255, 0.1);
 
-  color:white;
+  color: white;
 }
 
 /* rank label */
 
-.rank-label{
-  position:absolute;
+.rank-label {
+  position: absolute;
   top: 0px;
-  left:50%;
-  transform:translateX(-50%);
+  left: 50%;
+  transform: translateX(-50%);
 
   padding: 0px 10px;
-  border-radius:20px;
-  font-size:10px;
-  font-weight:bold;
+  border-radius: 20px;
+  font-size: 10px;
+  font-weight: bold;
 
-  background:#333;
+  background: #333;
 }
 
-.icon{
+.icon {
   /* font-size:50px;
   margin:20px 0; */
 }
 
-.title{
-  font-size:10px;
-  font-weight:bold;
+.title {
+  font-size: 10px;
+  font-weight: bold;
 }
 
-.subtitle{
-  font-size:10px;
-  opacity:0.7;
+.subtitle {
+  font-size: 10px;
+  opacity: 0.7;
 }
 
-.price{
-  margin-top:12px;
-  padding:4px 10px;
-  border-radius:20px;
-  font-size:10px;
+.price {
+  margin-top: 12px;
+  padding: 4px 10px;
+  border-radius: 20px;
+  font-size: 10px;
 }
 
 /* ================= */
 /* COMMON */
 /* ================= */
 
-.common{
-  border-color:#9e7b5c;
+.common {
+  border-color: #9e7b5c;
 }
 
-.common .rank-label{
-  background:#6d4a2f;
+.common .rank-label {
+  background: #6d4a2f;
 }
 
-.common .price{
-  background:#4a2d17;
+.common .price {
+  background: #4a2d17;
 }
+
+/* ================= */
+/* UNCOMMON */
+/* ================= */
+
+.uncommon {
+  border-color: #85d284;
+}
+
+.uncommon .rank-label {
+  background: #2bdf3a;
+}
+
+.uncommon .price {
+  background: #0a4c0b;
+}
+
 
 /* ================= */
 /* RARE */
 /* ================= */
 
-.rare{
-  border-color:#8d5cff;
+.rare {
+  border-color: #8d5cff;
 
   box-shadow:
-  0 0 10px rgba(140,90,255,0.7);
+    0 0 10px rgba(140, 90, 255, 0.7);
 }
 
-.rare .rank-label{
-  background:#7d5cff;
+.rare .rank-label {
+  background: #7d5cff;
 }
 
-.rare .price{
-  background:#5036a6;
+.rare .price {
+  background: #5036a6;
 }
 
 /* ================= */
 /* LEGENDARY */
 /* ================= */
 
-.legendary{
-  border-color:#ffd46b;
+.legendary {
+  border-color: #ffd46b;
 
   box-shadow:
-  0 0 10px rgba(255,210,100,0.9),
-  0 0 30px rgba(255,210,100,0.4);
+    0 0 10px rgba(255, 210, 100, 0.9),
+    0 0 30px rgba(255, 210, 100, 0.4);
 }
 
-.legendary .rank-label{
-  background:#f3c55a;
-  color:#4a2a00;
+.legendary .rank-label {
+  background: #f3c55a;
+  color: #4a2a00;
 }
 
-.legendary .price{
-  background:#5c3b0a;
+.legendary .price {
+  background: #5c3b0a;
 }
-
 </style>
--- pages/shop/index.vue
+++ pages/shop/index.vue
@@ -15,9 +15,28 @@
             </div>
 
             <div class="shop-content">
-                <ItemCard rank="common" icon="🪷" title="Lotus Lamp" subtitle="Đèn Sen Xá Lợi" price="50 Merit" />
-                <ItemCard rank="rare" icon="🪷" title="Lotus Lamp" subtitle="Đèn Sen Xá Lợi" price="50 Merit" />
-                <ItemCard rank="legendary" icon="🪷" title="Lotus Lamp" subtitle="Đèn Sen Xá Lợi" price="50 Merit" />
+                <ItemCard :imagePath="goldenDonaldTrump" rank="legendary" title="Tượng vàng Trump"
+                    subtitle="xoá mọi khẩu nghiệp" price="50000 Merit" />
+                <ItemCard :imagePath="legendBell" rank="legendary" title="Chuông thần" subtitle="tăng x2 điểm khi gõ mõ"
+                    price="50 Merit" />
+                <ItemCard :imagePath="lotus" rank="legendary" title="Hoa sen bảo vật" subtitle="+2000 điểm tịnh tâm"
+                    price="50 Merit" />
+                <ItemCard :imagePath="prayerBead" rank="legendary" title="Chàng hạt bồ đề"
+                    subtitle="x2 trừ nghiệp khi gõ mõ" price="50 Merit" />
+                <ItemCard :imagePath="tamLinhSet" rank="legendary" title="Set đồ tâm linh"
+                    subtitle="x2 Strike merit khi gõ mõ" price="50 Merit" />
+
+                <ItemCard :imagePath="jadeRing" rank="rare" title="Vòng ngọc lục bảo" subtitle="+1500 điểm tịnh tâm"
+                    price="50 Merit" />
+                <ItemCard :imagePath="tuongPhatBoTat" rank="rare" title="Tượng phật ngọc lục bảo"
+                    subtitle="-5500 điểm khẩu nghiệp" price="50 Merit" />
+
+                <ItemCard :imagePath="diLacBarecet" rank="uncommon" title="Dây chuyền phật di lặc"
+                    subtitle="+15% điểm tịnh tâm" price="50 Merit" />
+
+                <ItemCard :imagePath="wheel" rank="common" title="Bánh xe" subtitle="+5% điểm tịnh tâm"
+                    price="50 Merit" />
+
             </div>
 
         </div>
@@ -33,5 +52,15 @@
 
 <script setup>
 const { isMobileView } = useDevice()
+import jadeRing from '../../assets/icon/shop/jadeRing.png';
+import legendBell from '../../assets/icon/shop/legendBell.png';
+import lotus from '../../assets/icon/shop/lotus.png';
+import prayerBead from '../../assets/icon/shop/prayerBead.png';
+import tamLinhSet from '../../assets/icon/shop/tamLinhSet.png';
+import wheel from '../../assets/icon/shop/wheel.png';
+import diLacBarecet from '../../assets/icon/shop/barecel.png';
+import tuongPhatBoTat from '../../assets/icon/shop/tuongPhatBoTat.png';
+import goldenDonaldTrump from '../../assets/icon/shop/goldenDonaldTrump.png';
+
 
 </script>
