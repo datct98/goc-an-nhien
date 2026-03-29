ce00d15daedccdba127b332d1cfccfaa5ff12333
sonht <mikechael74@gmail.com>
2026-03-29 21:56:10 +0700

    add swipe to slide page

--- pages/huyenHoc/index.vue
+++ pages/huyenHoc/index.vue
@@ -1,14 +1,30 @@
 <template>
     <div v-if="isMobileView" class="container-huyenhoc-mobile">
-        <button class="toggleButtonSetting" @click="toggleSetting()">
+        <!-- <button class="toggleButtonSetting" @click="toggleSetting()">
             <i class="pi pi-cog"></i>
-        </button>
+        </button> -->
 
         <!-- Giao diện màn tung đồng xu -->
-        <div class="coinPage">
-            <Coin ref="firstCoinRef" />
-            <button class="btnTossCoin" @click="tossCoin()"> Tung đồng xu </button>
-        </div>
+        <swiper class="mySwiper">
+            <swiper-slide>
+                <div class="coinPage">
+                    <p class="title">Gieo quẻ</p>
+                    <Coin ref="firstCoinRef" />
+                    <button class="btnTossCoin" @click="tossCoin()"> Tung đồng xu </button>
+                </div>
+            </swiper-slide>
+            <swiper-slide>
+                <div class="tarotPage">
+                    <p class="title">Xem tarot</p>
+                </div>
+            </swiper-slide>
+            <swiper-slide>
+                <div class="cunghoangdaoPage">
+                    <p class="title">Xem Cung Hoàng Đạo</p>
+                </div>
+            </swiper-slide>
+        </swiper>
+
     </div>
     <div v-else class="container-huyenHoc">
         <div class="left-task-bar">
@@ -20,6 +36,11 @@
 
 <script setup>
 import Coin from '~/components/Coin.vue';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+// import Swiper and modules styles
+import 'swiper/css';
+
+
 const isMobileView = useDevice();
 
 const firstCoinRef = ref(null);
