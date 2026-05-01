3a7ca4167139a6b8212eccba6af040bf5edc3be2
datct98 <kiwisun1998@gmail.com>
2026-05-01 22:04:37 +0700

    fix /goMO + tienIch

--- pages/tien-ich/chia-tien/index.vue
+++ pages/tien-ich/chia-tien/index.vue
@@ -10,10 +10,13 @@ import SplitBillMobile from '~/components/split-bill/SplitBillMobile.vue';
 
 <style scoped>
 .chia-tien-wrapper {
-    width: 100%;
-    max-width: 448px;
-    margin: 0 auto;
-    min-height: 100dvh;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    justify-content: center;
     background-color: #0d0705;
     overflow: hidden;
     overscroll-behavior: none;
