adaa587adaebee6aebaaadb93a3c15c36ae712fc
Hoang Son <mikechael74@gmail.com>
2026-09-05 12:13:46 +0700

    fix bug

--- pages/goMo/index.vue
+++ pages/goMo/index.vue
@@ -11,13 +11,13 @@
       <!-- Status -->
       <div class="status-container">
         <div class="status-text">
-          Công đức : {{ stats.merit }}
+          Công đức {{ stats.merit }}
         </div>
         <div class="status-text">
-          Tâm tịnh : {{ stats.peace }}
+          Tâm tịnh {{ stats.peace }}
         </div>
         <div class="status-text">
-          Nghiệp tiêu : {{ stats.karma }}
+          Nghiệp tiêu {{ stats.karma }}
         </div>
       </div>
       <!-- Container tượng phật & 3 quả cầu -->
